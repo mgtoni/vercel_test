34a1c73a41c47ac514c68536a25db74b1094808c
mgtoni <toni.mares@yahoo.com>
2025-09-16 15:02:03 +0100

    fixed upabase utils path

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,6 +1,7 @@
 """Utility helpers used by the API routes.
 
-Re-export common helpers for convenience.
+Re-export commonly used helpers for convenience.
+Avoid importing modules that may not exist after refactors.
 """
 
 from .common import normalize_email
@@ -10,11 +11,11 @@ from .crypto_utils import (
     aesgcm_encrypt_profile,
     mask_email_for_log,
 )
-from .supabase_utils import (
+from .core_supabase import (
     build_supabase_public,
     admin_get_user_by_email_rest,
     fetch_profile_admin_sdk,
-    check_email_exists_rest,
+    create_signed_storage_url,
 )
 
 __all__ = [
@@ -26,6 +27,5 @@ __all__ = [
     "build_supabase_public",
     "admin_get_user_by_email_rest",
     "fetch_profile_admin_sdk",
-    "check_email_exists_rest",
+    "create_signed_storage_url",
 ]
-
